5227f7679419e7fd29c9a309ffde0f160b0b9b43
Andreas Kloeckner <inform@tiker.net>
2008-12-15 15:55:09 -0500

    Implement support for specifying BOOST_PYTHON_MAX_ARITY.

--- src/bpl.py
+++ src/bpl.py
@@ -3,10 +3,12 @@
 
 
 class BoostPythonModule(object):
-    def __init__(self, name="module"):
+    def __init__(self, name="module", max_arity=None):
         self.name = name
         self.mod_body = []
         self.init_body = []
+
+        self.max_arity = max_arity
 
     def add_to_init(self, body):
         self.init_body.extend(body)
@@ -23,12 +25,18 @@ class BoostPythonModule(object):
                         func.fdecl.name, func.fdecl.name)))
 
     def generate(self):
-        from codepy.cgen import Block, Module, Include, Line
-        return Module(
-            [Include("boost/python.hpp"), Line()]
-            + self.mod_body
-            + [Line(), Line("BOOST_PYTHON_MODULE(%s)" % self.name)]
-            + [Block(self.init_body)])
+        from codepy.cgen import Block, Module, Include, Line, Define
+        body = []
+
+        if self.max_arity is not None:
+            body.append(Define("BOOST_PYTHON_MAX_ARITY", self.max_arity))
+        
+        body += ([Include("boost/python.hpp"), Line()]
+                + self.mod_body
+                + [Line(), Line("BOOST_PYTHON_MODULE(%s)" % self.name)]
+                + [Block(self.init_body)])
+
+        return Module(body)
 
     def compile(self, platform, **kwargs):
         from codepy.libraries import add_boost_python
