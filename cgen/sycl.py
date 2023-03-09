d175fba85a142a158ee0767b00337b252da88368
pubuduprabhathiya <pubuduprabhathiya@gmail.com>
2023-03-09 14:31:18 +0530

    Change SYCLKernel, SYCLBody functions

--- cgen/sycl.py
+++ cgen/sycl.py
@@ -20,15 +20,15 @@ THE SOFTWARE.
 
 import numpy as np
 
-from cgen import \
-        Declarator, \
-        DeclSpecifier, \
-        NestedDeclarator, \
-        Value,\
-        Generable,\
-        FunctionBody,\
-        Lamda,\
-        Block
+from cgen import (
+    Block,
+    DeclSpecifier,
+    FunctionBody,
+    Generable,
+    Lamda,
+    NestedDeclarator,
+    Value,
+)
 
 
 def dtype_to_cltype(dtype):
@@ -62,51 +62,57 @@ def dtype_to_cltype(dtype):
 
 # {{{ kernel
 
+
 class SYCLKernel(DeclSpecifier):
-    def __init__(self, subdecl,ndim):
-        self.ndim=ndim
-        subdecl.arg_decls.append(Value("sycl::queue","queue_"))
-        subdecl.arg_decls.append(Value("sycl::nd_range<{}> ".format(self.ndim),"range_"))
-        DeclSpecifier.__init__(self, subdecl,"")
+    def __init__(self, subdecl, ndim, ndrange, queue):
+        self.ndim = ndim
+        subdecl.arg_decls.append(Value("sycl::queue", queue))
+        subdecl.arg_decls.append(
+            Value("sycl::nd_range<{}> ".format(self.ndim), ndrange)
+        )
+        DeclSpecifier.__init__(self, subdecl, "")
 
     mapper_method = "map_sycl_kernel"
+
 
 # }}}
 
 
 # {{{ kernel args
 
+
 class SYCLConstant(DeclSpecifier):
     def __init__(self, subdecl):
-        self.subdecl=subdecl
+        self.subdecl = subdecl
 
     def get_decl_pair(self):
-        sub_tp,sub_decl=self.subdecl.get_decl_pair()
-        return [f"sycl::constant_ptr<{sub_tp[0]}>"],sub_decl
-    
+        sub_tp, sub_decl = self.subdecl.get_decl_pair()
+        return [f"sycl::constant_ptr<{sub_tp[0]}>"], sub_decl
+
     mapper_method = "map_sycl_constant"
 
 
 class SYCLLocal(NestedDeclarator):
     def __init__(self, subdecl):
-        self.subdecl=subdecl
+        self.subdecl = subdecl
 
     def get_decl_pair(self):
-        sub_tp,sub_decl=self.subdecl.get_decl_pair()
-        return [f"sycl::local_ptr<{sub_tp[0]}>"],sub_decl
-    
+        sub_tp, sub_decl = self.subdecl.get_decl_pair()
+        return [f"sycl::local_ptr<{sub_tp[0]}>"], sub_decl
+
     mapper_method = "map_sycl_local"
 
 
 class SYCLGlobal(DeclSpecifier):
     def __init__(self, subdecl):
-        self.subdecl=subdecl
+        self.subdecl = subdecl
 
     def get_decl_pair(self):
-        sub_tp,sub_decl=self.subdecl.get_decl_pair()
-        return [f"sycl::global_ptr<{sub_tp[0]}>"],sub_decl
-    
+        sub_tp, sub_decl = self.subdecl.get_decl_pair()
+        return [f"sycl::global_ptr<{sub_tp[0]}>"], sub_decl
+
     mapper_method = "map_sycl_global"
+
 
 # TODO sycl Image
 class SYCLImage(Value):
@@ -122,6 +128,7 @@ class SYCLImage(Value):
 
     mapper_method = "map_cl_image"
 
+
 # }}}
 
 
@@ -132,62 +139,87 @@ class _SYCLWorkGroupSizeDeclarator(Generable):
         while len(dim) < 3:
             dim = dim + (1,)
         self.dim = dim
-        self.decl='extern "C" [[sycl::reqd_work_group_size({})]]'.format(", ".join(str(d) for d in self.dim))
+        self.decl = 'extern "C" [[sycl::reqd_work_group_size({})]]'.format(
+            ", ".join(str(d) for d in self.dim)
+        )
 
     def generate(self):
         yield self.decl
+
+
 class SYCLRequiredWorkGroupSize(_SYCLWorkGroupSizeDeclarator):
     mapper_method = "map_SYCL_required_wokgroup_size"
 
+
 # }}}
 
-#TODO SYCL vector
+# TODO SYCL vector
 # {{{ vector PODs
 
 # }}}
 class SYCLAccessor(NestedDeclarator):
-    def __init__(self, subdecl,handler,count=None):
+    def __init__(self, subdecl, handler, count=None):
         NestedDeclarator.__init__(self, subdecl)
-        self.handler=handler
-        self.count=count
+        self.handler = handler
+        self.count = count
         sub_tp, sub_decl = subdecl.get_decl_pair()
-        self.type=sub_tp[0]
-        self.sub_decl=sub_decl
+        self.type = sub_tp[0]
+        self.sub_decl = sub_decl
 
     def get_decl_pair(self):
         if self.count is None:
             return [f"sycl::accessor<{ self.type}>"], f"{self.sub_decl}({self.handler})"
         else:
-            return [f"sycl::accessor<{ self.type}>"], f"{self.sub_decl}(sycl::range<1>({self.count}),{self.handler})"
+            return [
+                f"sycl::accessor<{ self.type}>"
+            ], f"{self.sub_decl}(sycl::range<1>({self.count}),{self.handler})"
 
     def get_type(self):
         return self.type
+
     mapper_method = "map_sycl_accessor"
 
-    
+
 class SYCLBody(Generable):
-    def __init__(self, body,ndim,handler,nd_item):
-        self.body =SYCLQueueSubmit(Block([SYCLparallel_for(body,ndim,handler,nd_item)]),handler)
+    def __init__(self, body, ndim, handler, nd_item, queue, ndrange):
+        self.body = SYCLQueueSubmit(
+            Block([SYCLparallel_for(body, ndim, handler, nd_item, ndrange)]),
+            handler,
+            queue,
+        )
 
     def generate(self):
         yield from self.body.generate()
 
     mapper_method = "map_SYCL_function_body"
 
+
 class SYCLparallel_for(Generable):
-    def __init__(self, body,ndim,handler,nd_item):
-        self.handler=handler
-        self.args=["range_",FunctionBody(Lamda("=",[Value(f"sycl::nd_item<{ndim}>",nd_item)]),body)]
+    def __init__(self, body, ndim, handler, nd_item, ndrange):
+        self.handler = handler
+        self.args = [
+            ndrange,
+            FunctionBody(Lamda("=", [Value(f"sycl::nd_item<{ndim}>", nd_item)]), body),
+        ]
 
     def generate(self):
-        yield "{}.parallel_for({});".format(self.handler,", ".join(str(ad) for ad in self.args))
+        yield "{}.parallel_for({});".format(
+            self.handler, ", ".join(str(ad) for ad in self.args)
+        )
 
     mapper_method = "map_SYCL_parallel_for"
+
+
 class SYCLQueueSubmit(Generable):
-    def __init__(self, body,handler):
-        self.args=[FunctionBody(Lamda("&",[Value("sycl::handler &", handler)]),body)]
+    def __init__(self, body, handler, queue):
+        self.queue = queue
+        self.args = [
+            FunctionBody(Lamda("&", [Value("sycl::handler &", handler)]), body)
+        ]
 
     def generate(self):
-        yield "queue_.submit({}).wait();".format(", ".join(str(ad) for ad in self.args))
+        yield "{}.submit({}).wait();".format(
+            self.queue, ", ".join(str(ad) for ad in self.args)
+        )
 
     mapper_method = "map_SYCL_queue_submit"
