fd857b3328c4512295df737747e4d87f3496fe67
Alexandru Fikl <alexfikl@gmail.com>
2024-07-17 10:59:10 +0300

    version: use importlib

--- cgen/version.py
+++ cgen/version.py
@@ -1,3 +1,15 @@
-VERSION = (2020, 2)
-VERSION_STATUS = ""
-VERSION_TEXT = ".".join(str(x) for x in VERSION) + VERSION_STATUS
+from importlib import metadata
+from typing import Tuple
+
+
+def _parse_version(version: str) -> Tuple[Tuple[int, ...], str]:
+    import re
+
+    m = re.match("^([0-9.]+)([a-z0-9]*?)$", VERSION_TEXT)
+    assert m is not None
+
+    return tuple(int(nr) for nr in m.group(1).split(".")), m.group(2)
+
+
+VERSION_TEXT = metadata.version("cgen")
+VERSION, VERSION_STATUS = _parse_version(VERSION_TEXT)
